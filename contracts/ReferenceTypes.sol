c724ed17fb52925c22c5472da9cedf71af6424b3
Ricardo <ricardoinliner@yahoo.com>
2021-04-08 19:38:29 -0300

    data types comments

--- contracts/ReferenceTypes.sol
+++ contracts/ReferenceTypes.sol
@@ -3,6 +3,17 @@ pragma solidity ^0.7.0;
 
 contract ReferenceTypes {
 
+    /*
+        There are three data location: memory, storage and calldata
+        - memory: is a data location whose lifetime is temporary
+        - storage: is a data location where the state variables are stored
+        - calldata: is a special data location that contains the function arguments
+        - calldata is imutable
+        - where you are using external, you should use calldata instead of storage
+        - you should use memory wherever it is possible to save gas
+
+    */
+
     // array type is reference
     uint[] public x = [1,2,3,4,5,6];
 
